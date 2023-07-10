b7805aff4eb117dde5f049824d0d625b953f4ecc
ioanadev <119631887+ioanadev@users.noreply.github.com>
2023-07-10 21:53:50 +0300

    some stylizations

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,14 @@ import { Section } from './Title/tittle';
 
 export const App = () => {
   return (
-    <div>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'left',
+        marginLeft: 50,
+      }}
+    >
       <Section title="Please leave feedback">
         <Counter />
       </Section>
